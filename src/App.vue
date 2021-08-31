05c86f47d280552e0e9533e57a6c0cdbbfd00add
kiketion <kiketionje@gmail.com>
2021-08-31 18:37:45 -0500

    creating Vue Crypto API

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>HelloJack!</h1>
+
+  <table>
+    <thead>
+      <tr>
+        <th>coin</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="coin in coins" :key="coin.id">
+        {{
+          coin.name
+        }}
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      coins: [],
+    };
+  },
+
+  async mounted() {
+    const res = await fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    );
+    const data = await res.json();
+    console.log(data);
+    this.coins = data;
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
